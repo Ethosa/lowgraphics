1ccf4e0648291dc1d498ee75ed6acf13afbc69a7
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-12-24 18:41:07 +0700

    new test

--- test.cpp
+++ test.cpp
@@ -11,6 +11,10 @@ int main(int argc, char const *argv[])
 
     img.line(0, 0, 256, 256, 0x99dd7777);
     img.rect(7, 7, 55, 55, 0x757777dd);
+    img.circle(128, 128, 32, 0xFFFFFFFF);
+
+    img.line(128, 64, 64, 128, image::parse_color(32, 64, 128));
+
     img.save("file.bmp", 0);
     std::cout << clock() - now << " times." << std::endl;
     return 0;
