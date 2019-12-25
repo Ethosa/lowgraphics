b81c942e5e84629f6e7e31bf7deaf5e2d2b8c035
Ethosa <49402667+Ethosa@users.noreply.github.com>
2019-12-26 02:06:43 +0700

    Color.cpp, Color.h - code extend

--- lowgraphics/graphics/Color.h
+++ lowgraphics/graphics/Color.h
@@ -1,9 +1,13 @@
 // author: Ethosa
 #include <iostream>
+#include <locale>
+#include <map>
 
 class Color
 {
 public:
+    typedef std::map<std::string, int> _colors_map;
+    static _colors_map colors_map;
     static int parse_color(int r, int g, int b, int a);
     static int parse_color(int r, int g, int b);
     static int parse_color(std::string s);
